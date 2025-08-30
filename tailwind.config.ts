bf57a1961c7e311dba66133728a686be385c77a4
Linkeshvaran Raja Krishna Kumar <linkeshvaranr@gmail.com>
2025-08-30 16:59:30 +0530

    test2

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,11 +3,11 @@ import typography from '@tailwindcss/typography'
 
 const config: Config = {
   content: [
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/**/*.{md,mdx}", // for markdown content
-  ],
+  "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+  "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+  "./src/content/**/*.{md,mdx}",
+],
   theme: {
     extend: {},
   },
