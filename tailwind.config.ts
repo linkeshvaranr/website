1f4fa69dde7adde213ffc7314741f0056462b016
Linkeshvaran Raja Krishna Kumar <linkeshvaranr@gmail.com>
2025-08-30 16:46:23 +0530

    fixed tailwind config ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,11 +3,15 @@ import typography from '@tailwindcss/typography'
 
 const config: Config = {
   content: [
-    './src/**/*.{js,ts,jsx,tsx,mdx,md}',
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{md,mdx}", // for markdown content
   ],
   theme: {
     extend: {},
   },
   plugins: [typography],
 }
+
 export default config
